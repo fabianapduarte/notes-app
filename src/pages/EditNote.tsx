0c1ffba1f313bdd0bff0876e45174b04db571947
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2021-04-05 21:57:36 -0300

    add update method

--- src/pages/EditNote.tsx
+++ src/pages/EditNote.tsx
@@ -1,12 +1,63 @@
+import { useState, useEffect } from 'react';
 import { FiArrowLeft, FiCheck, FiX } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
+import { Link, RouteComponentProps, useHistory } from 'react-router-dom';
+
 import FormNote from '../components/FormNote';
+import { DataNotesProps } from '../utils/NoteInterface';
 
-import data from '../data.json';
+interface ParamsProps {
+  id: string;
+}
 
-const dataNotes = data;
+function EditNote(props: RouteComponentProps<ParamsProps>) {
+  const history = useHistory();
 
-function EditNote() {
+  let dataNotes: DataNotesProps[] = JSON.parse(localStorage.getItem("notes")!);
+
+  const [note, setNote] = useState<DataNotesProps | null>(null);
+  const [loading, setLoading] = useState<Boolean>(true);
+
+  const noteIdUrl = props.match.params.id;
+
+  function loadingNote(noteIdUrl: string) {
+    dataNotes.forEach(note => {
+      if (note.id === Number(noteIdUrl)) {
+        setNote({
+          id: note.id,
+          title: note.title,
+          text: note.text
+        });
+      }
+    });
+
+    setLoading(false);
+  }
+  
+  useEffect(() => {
+    loadingNote(noteIdUrl);
+  }, [noteIdUrl]);
+
+  function redirectToHome() {
+    alert("A nota não foi encontrada");
+    history.push("/");
+  }
+
+  function updateNote(idNote: string) {
+    const updatedTitle = (document.getElementById("title") as HTMLInputElement)?.value || (document.getElementById("title") as HTMLInputElement)?.defaultValue;
+    const updatedText = (document.getElementById("text") as HTMLInputElement)?.value || (document.getElementById("text") as HTMLInputElement)?.defaultValue; 
+
+    dataNotes.forEach(note => {
+      if (note.id === Number(idNote)) {
+        note.title = updatedTitle;
+        note.text = updatedText;
+      }
+    });
+
+    localStorage.setItem("notes", JSON.stringify(dataNotes));
+
+    history.push("/");
+  }
+
   return (
     <div className="container note-fullscreen">
       <header className="align-items-header">
@@ -15,23 +66,39 @@ function EditNote() {
         </Link>
 
         <div className="header-buttons">
-          <button className="btn-gray">
-            <FiX size={16} />
-            <span>Cancelar</span>
-          </button>
+          <Link to="/">
+            <button className="btn-gray">
+              <FiX size={16} />
+              <span>Cancelar</span>
+            </button>
+          </Link>
 
-          <button type="submit" form="form-note" className="btn-green">
+          <button className="btn-green" onClick={() => updateNote(noteIdUrl)}>
             <FiCheck size={16} />
             <span>Salvar nota</span>
           </button>
         </div>
       </header>
 
-      <FormNote
-        className="form-card-note"
-        title={dataNotes.notes[0].title}
-        text={dataNotes.notes[0].text}
-      />
+      {loading === false
+        ?
+        (
+          note
+          ?
+          (
+            <FormNote
+              key={note.id}
+              className="form-card-note"
+              title={note.title}
+              text={note.text}
+            />
+          )
+          :
+          (redirectToHome())
+        )
+        :
+        console.log('loading')
+      }
     </div>
   );
 }
