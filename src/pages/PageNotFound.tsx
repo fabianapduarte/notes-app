3b70f5cf2700e34e53855b63a7b3cde47bace757
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2021-04-08 23:51:16 -0300

    create page not found

---
+++ src/pages/PageNotFound.tsx
@@ -0,0 +1,9 @@
+import Modal from "../components/Modal";
+
+function PageNotFound() {
+  return (
+    <Modal text="Página não encontrada" />
+  )
+}
+
+export default PageNotFound;
--- src/routes.tsx
+++ src/routes.tsx
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 import SeeNote from './pages/SeeNote';
 import AddNote from './pages/AddNote';
 import EditNote from './pages/EditNote';
+import PageNotFound from './pages/PageNotFound';
 
 function Routes() {
   return (
@@ -14,6 +15,7 @@ function Routes() {
         <Route path="/note/:id" component={SeeNote} />
         <Route path="/add" component={AddNote} />
         <Route path="/edit/:id" component={EditNote} />
+        <Route path="*" component={PageNotFound} />
       </Switch>
     </BrowserRouter>
   );
